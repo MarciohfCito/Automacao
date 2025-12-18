9c909ec54574c8f6a608b30a0bdcb3c41a6270fd
Márcio Honorindo Freires Citó <marcio.cito@aged.ma.gov.br>
2025-12-18 13:33:09 -0300

    primeiro protótipo - automação de cadastro do SIGAMA

--- teste.py
+++ teste.py
@@ -1,4 +1,5 @@
 import pyautogui
+import pyperclip
 import time
 
 #inserir o numero de registros
@@ -11,35 +12,139 @@ def minimizar():
 
 #minimizar vscode
 minimizar()
-
-#copiar - SIGAMA
-pyautogui.moveTo(x=128, y=17)
-pyautogui.click()
-pyautogui.moveTo(x=38, y=335)
-pyautogui.tripleClick()
-pyautogui.hotkey('ctrl', 'c')
 time.sleep(0.5)
 
-# x, y = pyautogui.locateCenterOnScreen('excel_image.png', confidence = 0.8)
-# print(x)
-# print(y)
-pyautogui.click(pyautogui.locateCenterOnScreen('excel_image.png', confidence = 0.8))
-time.sleep(0.5)
+#POSIÇÕES
+posicao_i_nome_S = [77, 336]
+posicao_a_nome_S = posicao_i_nome_S
 
-pyautogui.moveTo(x=207, y=468)
-pyautogui.hotkey('ctrl', 'v')
+posicao_i_nome_E = [252, 309]
+posicao_a_nome_E = posicao_i_nome_E
 
-#minimizar excel
-minimizar()
-pyautogui.moveTo(x=835, y=332)
-pyautogui.doubleClick()
+posicao_i_cpf_S = [839, 336]
+posicao_a_cpf_S = posicao_i_cpf_S
 
-pyautogui.hotkey('ctrl', 'c')
+posicao_i_cpf_E = [590, 309]
+posicao_a_cpf_E = posicao_i_cpf_E
+posicao_i_lupa = [1690, 336]
+posicao_a_lupa = posicao_i_lupa
 
-pyautogui.click(pyautogui.locateCenterOnScreen('excel_image.png', confidence = 0.8))
-time.sleep(0.5)
-pyautogui.moveTo(x=548, y=471)
-pyautogui.click()
-pyautogui.hotkey('ctrl', 'v')
+for j in range(num):
 
-time.sleep(0.5)
+    #copiar nome - SIGAMA
+    pyautogui.moveTo(posicao_a_nome_S)
+    pyautogui.tripleClick()
+    pyautogui.hotkey('ctrl', 'c')
+    time.sleep(0.5)
+
+    #localizar excel
+    pyautogui.click(pyautogui.locateCenterOnScreen('excel_image.png', confidence = 0.8))
+    time.sleep(0.5)
+
+    #colar nome EXCEL
+    pyautogui.moveTo(posicao_a_nome_E)
+    pyautogui.click()
+    time.sleep(0.5)
+    pyautogui.hotkey('ctrl', 'v')
+
+    #localizar chrome
+    pyautogui.click(pyautogui.locateCenterOnScreen('chrome_image.png', confidence = 0.8))
+
+    #copiar cpf SIGAMA
+    pyautogui.moveTo(posicao_a_cpf_S)
+    pyautogui.doubleClick()
+    pyautogui.hotkey('ctrl', 'c')
+
+    #localizar excel
+    pyautogui.click(pyautogui.locateCenterOnScreen('excel_image.png', confidence = 0.8))
+
+    #colar cpf EXCEL
+    pyautogui.moveTo(posicao_a_cpf_E)
+    pyautogui.click()
+    time.sleep(0.2)
+    pyautogui.click()
+    pyautogui.hotkey('_')
+    time.sleep(0.2)
+    pyautogui.hotkey('ctrl', 'v')
+
+    #copiar nome e cpf EXCEL
+    pyautogui.moveTo(posicao_a_nome_E)
+    pyautogui.click()
+    time.sleep(0.5)
+    drag_x, drag_y = posicao_a_cpf_E
+    pyautogui.dragTo(drag_x, drag_y, 0.2, button='left')
+    pyautogui.hotkey('ctrl', 'c')
+    texto = pyperclip.paste()
+
+    texto_tratado = texto.replace("\t","").replace("\n", "").replace("\r", "")
+
+    #abrir gerenciador de arquivos e criar nova pasta
+    pyautogui.click(pyautogui.locateCenterOnScreen('arquivos_image.png', confidence = 0.8))
+    time.sleep(0.5)
+    pyautogui.click(pyautogui.locateCenterOnScreen('nova_pasta.png', confidence = 0.8))
+    time.sleep(0.5)
+    pyautogui.click(pyautogui.locateCenterOnScreen('pasta.png', confidence = 0.8))
+    pyautogui.write(texto_tratado, interval=0.05)
+    pyautogui.hotkey('Enter')
+
+    #localizar chrome
+    pyautogui.click(pyautogui.locateCenterOnScreen('chrome_image.png', confidence = 0.8))
+
+    #localizar lupa
+    pyautogui.moveTo(posicao_a_lupa)
+    pyautogui.click()
+    time.sleep(1)
+
+    #localizar documentos
+    pyautogui.moveTo(pyautogui.locateCenterOnScreen('anexo_image.png', confidence = 0.8))
+    x1, y1 = pyautogui.locateCenterOnScreen('anexo_image.png', confidence = 0.8)
+
+    #clicar no doc
+    pyautogui.moveTo(x1, y1+27)
+    pyautogui.click()
+
+    for i in range(4):
+        y1 = y1+23
+        pyautogui.moveTo(x1, y1)
+        time.sleep(0.5)
+        pyautogui.click()
+        time.sleep(0.2)
+
+    pyautogui.click(pyautogui.locateCenterOnScreen('X_image.png', confidence = 0.8))
+
+    #abrir gerenciador de arquivos e downloads
+    pyautogui.click(pyautogui.locateCenterOnScreen('arquivos_image.png', confidence = 0.8))
+    time.sleep(0.5)
+    pyautogui.click(pyautogui.locateCenterOnScreen('download_image.png', confidence = 0.8))
+    time.sleep(0.5)
+    pyautogui.click(pyautogui.locateCenterOnScreen('hoje_image.png', confidence = 0.8))
+    time.sleep(1)
+    pyautogui.hotkey('ctrl', 'c')
+    pyautogui.click(pyautogui.locateCenterOnScreen('seta_voltar_image.png', confidence = 0.8))
+    time.sleep(0.5)
+    pyautogui.hotkey('Enter')
+    time.sleep(0.5)
+    pyautogui.hotkey('ctrl', 'v')
+    pyautogui.click(pyautogui.locateCenterOnScreen('download_image.png', confidence = 0.8))
+    time.sleep(0.5)
+    pyautogui.click(pyautogui.locateCenterOnScreen('hoje_image.png', confidence = 0.8))
+    time.sleep(1)
+    pyautogui.hotkey('Delete')
+    time.sleep(0.5)
+    pyautogui.click(pyautogui.locateCenterOnScreen('seta_voltar_image.png', confidence = 0.8))
+    time.sleep(0.5)
+    pyautogui.click(pyautogui.locateCenterOnScreen('seta_voltar_image.png', confidence = 0.8))
+    time.sleep(0.5)
+
+    #localizar chrome
+    pyautogui.click(pyautogui.locateCenterOnScreen('chrome_image.png', confidence = 0.8))
+
+    posicao_a_nome_S[1] = posicao_a_nome_S[1] + 45
+
+    posicao_a_nome_E[1] = posicao_a_nome_E[1] + 26
+
+    posicao_a_cpf_S[1] = posicao_a_cpf_S[1] + 45
+
+    posicao_a_cpf_E[1] = posicao_a_cpf_E[1] + 26
+
+    posicao_a_lupa[1] = posicao_a_lupa[1] + 45
